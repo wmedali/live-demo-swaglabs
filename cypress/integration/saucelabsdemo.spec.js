1c5adb12485f55195db9bc80994681e8ddb5bf07
Ali <Ali.MALHOUF@consulting-for.edenred.com>
2021-11-09 17:26:21 +0100

    initial commit

---
+++ cypress/integration/saucelabsdemo.spec.js
@@ -0,0 +1,43 @@
+describe(" Sauce Demo - Authentication Test Suite ", () => {
+  beforeEach(() => {
+    cy.visit("https://www.saucedemo.com/");
+  });
+
+  it("Scenario 1 - Valid username and password", () => {
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+    cy.url().should("include", "inventory");
+    cy.get(".inventory_item").should("have.length", 6).and("be.visible");
+  });
+
+  it("Scenario 2 - Valid username and invalid password", () => {
+    cy.get("[data-test=username]").type("standard_user");
+    cy.get("[data-test=password]").type("secret_s");
+    cy.get("[data-test=login-button]").click();
+
+    cy.get("[data-test=error]")
+      .should("be.visible")
+      .and("include.text", "Username and password do not match any user");
+  });
+
+  it("Scenario 3 - Invalid username and valid password", () => {
+    cy.get("[data-test=username]").type("standard_u");
+    cy.get("[data-test=password]").type("secret_sauce");
+    cy.get("[data-test=login-button]").click();
+
+    cy.get("[data-test=error]")
+      .should("be.visible")
+      .and("include.text", "Username and password do not match any user");
+  });
+
+  it("Scenario 4 - Locked out user", () => {
+    cy.get("[data-test=username]").type("locked_out_user");
+    cy.get("[data-test=password]").type("secret_sauce");
+    cy.get("[data-test=login-button]").click();
+
+    cy.get("[data-test=error]")
+      .should("be.visible")
+      .and("include.text", "this user has been locked out");
+  });
+});
