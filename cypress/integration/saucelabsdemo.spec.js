93b81168ce83138eb1d74b21518a9a0d6c7093da
Ali <Ali.MALHOUF@consulting-for.edenred.com>
2021-11-09 17:43:16 +0100

    git modification

--- cypress/integration/saucelabsdemo.spec.js
+++ cypress/integration/saucelabsdemo.spec.js
@@ -1,9 +1,9 @@
-describe(" Sauce Demo - Authentication Test Suite ", () => {
+describe("Sauce Demo - Authentication Test Suite ", () => {
   beforeEach(() => {
     cy.visit("https://www.saucedemo.com/");
   });
 
-  it("Scenario 1 - Valid username and password", () => {
+  it(" SC1 - Valid username and password", () => {
     cy.get('[data-test="username"]').type("standard_user");
     cy.get('[data-test="password"]').type("secret_sauce");
     cy.get('[data-test="login-button"]').click();
@@ -11,7 +11,7 @@ describe(" Sauce Demo - Authentication Test Suite ", () => {
     cy.get(".inventory_item").should("have.length", 6).and("be.visible");
   });
 
-  it("Scenario 2 - Valid username and invalid password", () => {
+  it("SC2 - Valid username and invalid password", () => {
     cy.get("[data-test=username]").type("standard_user");
     cy.get("[data-test=password]").type("secret_s");
     cy.get("[data-test=login-button]").click();
@@ -21,7 +21,7 @@ describe(" Sauce Demo - Authentication Test Suite ", () => {
       .and("include.text", "Username and password do not match any user");
   });
 
-  it("Scenario 3 - Invalid username and valid password", () => {
+  it("SC3 - Invalid username and valid password", () => {
     cy.get("[data-test=username]").type("standard_u");
     cy.get("[data-test=password]").type("secret_sauce");
     cy.get("[data-test=login-button]").click();
@@ -31,7 +31,7 @@ describe(" Sauce Demo - Authentication Test Suite ", () => {
       .and("include.text", "Username and password do not match any user");
   });
 
-  it("Scenario 4 - Locked out user", () => {
+  it("SC4 - Locked out user", () => {
     cy.get("[data-test=username]").type("locked_out_user");
     cy.get("[data-test=password]").type("secret_sauce");
     cy.get("[data-test=login-button]").click();
